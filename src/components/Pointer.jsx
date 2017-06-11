6b31b37357e2db89c4bcd6025bcb1c8ac8f1a004
李钿 <ld@lidiandeMBP.lan>
2017-06-11 22:32:22 +0800

    增加 Pointer 组件

--- src/components/Pointer.jsx
+++ src/components/Pointer.jsx
@@ -2,10 +2,31 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
 class Pointer extends PureComponent {
+  static propTypes = {
+    length: PropTypes.number.isRequired,
+    index: PropTypes.number.isRequired,
+  }
 
   render() {
+    console.info("Point render")
+    
+    const {
+      length,
+      index
+    } = this.props
+    let i = 0, items = [];
+    for(i; i < length; i++){
+      if(i === index){
+        items.push(<span key={i} className="pointer on"></span>);
+      } else{
+        items.push(<span key={i} className="pointer"></span>);
+      }
+    }
+
     return (
-      <div/>
+      <div className="viewer-image-pointer">
+        {items}
+      </div>
     );
   }
 }
