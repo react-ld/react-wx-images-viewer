69ba803dcbe6580c3943b949095ddbd43c0d35bc
李钿 <ld@lidiandeMacBook-Pro.local>
2017-06-12 09:22:58 +0800

    修改查看触发

--- example/App.jsx
+++ example/App.jsx
@@ -42,13 +42,14 @@ class App extends Component {
     return (
       <div className="app">
         <div className="img-list">
-          {
+          <button onClick={this.openViewer.bind(this, 0)}>点击打开图片</button>
+          {/*{
             this.state.imags.map((item, index) => {
               return <div className="img" key={item}>
                 <img src={item} alt="" onClick={this.openViewer.bind(this, index)} width="100%" height="auto" className=""/> 
               </div>
             })
-          }
+          }*/}
         </div>
         {
           isOpen ? <WxImageViewer onClose={this.onClose} urls={this.state.imags} index={index}/> : ""
